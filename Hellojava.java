12f2c90cdea76668e633017d23a0509f4c537896
Madduri Aravind <patelavi547@gmail.com>
2023-03-02 16:49:28 +0530

    initial commit

---
+++ Hellojava.java
@@ -0,0 +1,14 @@
+import java.applet.*;
+import java.awt.*;
+public class Hellojava extends Applet
+ {
+    public void init()
+    {}
+    public void paint(Graphics g)
+    {
+        g.drawString("hello cse",200,300);
+        /*<applet code="Hellojava.java"width=300,height=30></applet> */
+    }
+    
+    
+}
